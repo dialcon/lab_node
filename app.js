559c186a40a95846bb2af6162b2ec77b2161e137
dialcon <dialcon@gmail.com>
2017-03-06 17:25:16 -0500

    modificación de app.js

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 var express = require("express");
-var app = express();
+var app = express(),
+    fs = require("fs"),
+    parser = require("./params_parser.js");
+render = require("./render_view");
+var p = parser.parseParams;
+var r = render.render;
 
-app.get ("/",function(req,res){
- res.send("hola mundo");
+app.get("/", function (req, res) {
+    if (req.url.indexOf("favicon.ico") > 0) {
+        return;
+    }
+    fs.readFile("./index.html", function (err, html) {
+        if (!err) {
+
+            var paramArray = [],
+                parameters = {};
+
+            var parameters = p(req);
+            var htmlRes = r(html, parameters);
+            res.writeHead(200, {
+                "Content-Type": "text/html"
+            });
+            res.write(htmlRes);
+        } else {
+            res.writeHead(500, {
+                "Content-Type": "text/html"
+            });
+            res.write("!Error" + err);
+        }
+        res.end();
+    });
 });
-
 app.listen(8080);
